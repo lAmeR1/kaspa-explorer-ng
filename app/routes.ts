dbeaf74cea2338aab765415053ac4b1e31b47234
lAmeR1 <lamer1@github.com>
2025-04-01 23:33:48 +0200

    Add account details route and component implementation
    Re-enabled the `accounts/:address` route and implemented a new `accountdetails.tsx` component. This includes displaying account balance, transactions, UTXOs, and a tabbed interface for related details.

--- app/routes.ts
+++ app/routes.ts
@@ -7,7 +7,7 @@ export default [
     route("transactions", "./routes/transactions.tsx"),
     route("transactions/:txId", "./routes/transactiondetails.tsx"),
     route("accounts", "./routes/accounts.tsx"),
-    // route("accounts/:address", "./routes/accountdetails.tsx"),
+    route("accounts/:address", "./routes/accountdetails.tsx"),
 
     route("donate", "./routes/donate.tsx"),
     route("*", "./routes/notfound.tsx"),
