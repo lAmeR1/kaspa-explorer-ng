2ddd5c70536180593303a6665a5ea6c56beab90d
lAmeR1 <lamer1@github.com>
2025-04-02 21:57:31 +0200

    Enhance header navigation with dropdown menus
    Added dropdown menus for "BlockDAG," "Assets," and "Analytics" in the header to improve navigation. Dropdowns reveal additional options like "Blocks," "Transactions," and "Accounts" on hover, enhancing user accessibility and interaction.

--- app/header/Header.tsx
+++ app/header/Header.tsx
@@ -32,19 +32,101 @@ const Header = ({expanded, setExpanded, ref}: {
 
             {typeof window !== "undefined" &&
                 window.location.pathname !== "/" && (
-                <SearchBox value={searchValue} className="hidden lg:flex lg:ml-4 lg:mr-8"
-                           onChange={setSearchValue}/>
-            )}
+                    <SearchBox value={searchValue} className="hidden lg:flex lg:ml-4 lg:mr-8"
+                               onChange={setSearchValue}/>
+                )}
 
-            <div className="hidden sm:flex flex-row gap-x-4 ms-auto">
-                <div className="hover:cursor-pointer p-2">
-                    <NavLink to={"/blocks"}>Blocks</NavLink>
+            <div className="hidden  sm:flex flex-row gap-x-4 ms-auto">
+                <div className="hover:cursor-pointer relative p-2 group hover:text-primary hover:fill-primary">
+                    <span className="flex flex-row justify-center items-center">BlockDAG <ChevronUp className="h-4 duration-200
+                        group-hover:fill-primary
+                          group-hover:block
+                          group-hover:rotate-180"/>
+                    </span>
+
+                    <div className="absolute
+                        hidden group-hover:flex
+                        flex-col
+                        -translate-x-1/2
+                        left-1/2
+                        mt-2
+                        h-6
+                        w-48"/>
+                    <div className="absolute
+                        hidden group-hover:flex
+                        flex
+                        flex-col
+                        -translate-x-1/2
+                        left-1/2
+                        w-48 bg-white text-black z-2 mt-4
+                     p-4 rounded-2xl shadow-[#00000012] shadow-[0px_4px_32px_0px]">
+                        <NavLink to="/blocks">
+                            <div className="w-full hover:bg-gray-25 py-2">
+                                Blocks
+                            </div>
+                        </NavLink>
+                        <NavLink to="/transactions">
+                            <div className="w-full hover:bg-gray-25 py-2">
+                                Transactions
+                            </div>
+                        </NavLink>
+                        <NavLink to="/blocks">
+                            <div className="w-full hover:bg-gray-25 py-2">
+                                Accounts
+                            </div>
+                        </NavLink>
+
+                    </div>
+
                 </div>
-                <div className="hover:cursor-pointer p-2">
-                    <NavLink to={"/transactions"}>Transactions</NavLink>
+                <div className="relative group hover:cursor-pointer p-2">
+                    <NavLink to={"/transactions"}>Assets</NavLink>
+                    <div className="absolute
+                        hidden group-hover:flex
+                        flex-col
+                        -translate-x-1/2
+                        left-1/2
+                        mt-2
+                        h-6
+                        w-48"/>
+                    <div className="absolute
+                        hidden group-hover:flex
+                        flex
+                        flex-col
+                        -translate-x-1/2
+                        left-1/2
+                        w-48 bg-white text-black z-2 mt-4
+                     p-4 rounded-2xl shadow-[#00000012] shadow-[0px_4px_32px_0px]">
+                        <div className="w-full hover:bg-gray-25 py-2">Blocks</div>
+                        <div className="w-full hover:bg-gray-25 py-2">Transactions</div>
+                        <div className="w-full hover:bg-gray-25 py-2">Accounts..</div>
+
+                    </div>
                 </div>
-                <div className="hover:cursor-pointer p-2">
-                    <NavLink to={"/accounts"}>Accounts</NavLink>
+                <div className="hover:cursor-pointer relative p-2 group hover:text-primary">
+                    <NavLink to={"/blocks"}>Analytics</NavLink>
+                    <div className="absolute
+                        hidden group-hover:flex
+                        flex-col
+                        -translate-x-1/2
+                        left-1/2
+                        mt-2
+                        h-6
+                        w-48"/>
+                    <div className="absolute
+                        hidden group-hover:flex
+                        flex
+                        flex-col
+                        -translate-x-1/2
+                        left-1/2
+                        w-48 bg-white text-black z-2 mt-4
+                     p-4 rounded-2xl shadow-[#00000012] shadow-[0px_4px_32px_0px]">
+                        <div className="w-full hover:bg-gray-25 py-2">Blocks</div>
+                        <div className="w-full hover:bg-gray-25 py-2">Transactions</div>
+                        <div className="w-full hover:bg-gray-25 py-2">Accounts</div>
+
+                    </div>
+
                 </div>
             </div>
             <div className="sm:hidden ms-auto">
@@ -69,7 +151,7 @@ const Header = ({expanded, setExpanded, ref}: {
         </div>}
         {!expanded && typeof window !== "undefined" &&
             window.location.pathname !== "/" && <SearchBox value={searchValue} className="lg:hidden w-full"
-                                 onChange={(e) => setSearchValue(e.target.value)}/>}
+                                                           onChange={(e) => setSearchValue(e.target.value)}/>}
 
         {expanded && <div className="flex flex-col gap-y-3 sm:hidden">
             <div className="">
