3230610620995c3a96771e3f2cc26fa0548b0095
lAmeR1 <lamer1@github.com>
2025-11-05 22:31:35 +0100

    Update table cell alignment to top in PageTable
    Changed the table cell alignment from default to `align-text-top` for improved vertical alignment. This enhances readability and provides a more consistent appearance across devices and screen resolutions.

--- app/PageTable.tsx
+++ app/PageTable.tsx
@@ -35,7 +35,7 @@ const PageTable = (props: PageTableProps) => {
                   <td className={`pt-2 md:hidden text-gray-500`}>{props.headers[cellNr] || ""}</td>
                 )}
                 <td
-                  className={`break-all pt-0 md:ps-4 md:last:pe-4 md:py-3 text-left xs:text-left md:last:text-right last:pb-4 ${props.additionalClassNames && (props.additionalClassNames[cellNr] || "")}`}
+                  className={`break-all align-text-top pt-0 md:ps-4 md:last:pe-4 md:py-3 text-left xs:text-left md:last:text-right last:pb-4 ${props.additionalClassNames && (props.additionalClassNames[cellNr] || "")}`}
                 >
                   {cell}
                 </td>
